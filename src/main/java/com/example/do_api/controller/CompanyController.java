7e647bc122cd552dee69e1b481f2f5d4e0c3252e
omar1234567898 <96495748+omar1234567898@users.noreply.github.com>
2024-11-15 20:17:59 +0100

    Update CompanyController.java

--- src/main/java/com/example/do_api/controller/CompanyController.java
+++ src/main/java/com/example/do_api/controller/CompanyController.java
@@ -17,28 +17,24 @@ public class CompanyController {
     @Autowired
     private CompanyService companyService;
 
-    // Create or Update a company
     @PostMapping("/")
     public ResponseEntity<Company> createCompany(@RequestBody Company company) {
         Company savedCompany = companyService.saveCompany(company);
         return new ResponseEntity<>(savedCompany, HttpStatus.CREATED);
     }
 
-    // Get company by ID
     @GetMapping("/{id}")
     public ResponseEntity<Company> getCompany(@PathVariable Long id) {
         Optional<Company> company = companyService.getCompany(id);
         return company.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    // Get all companies
     @GetMapping("/")
     public ResponseEntity<List<Company>> getAllCompanies() {
         List<Company> companies = companyService.getAllCompanies();
         return ResponseEntity.ok(companies);
     }
 
-    // Delete a company by ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCompany(@PathVariable Long id) {
         companyService.deleteCompany(id);
